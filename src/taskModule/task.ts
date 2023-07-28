8a016df2d01cd088009955b52974c7cfcbdfb49d
terryli710 <li.terry710@gmail.com>
2023-07-28 11:25:50 -0700

    ✨  define task

--- src/taskModule/task.ts
+++ src/taskModule/task.ts
@@ -1,4 +1,4 @@
-
+import { Boolean, Number as NumberRunType, String, Array, Record, Static, Partial, Literal, Union } from 'runtypes';
 
 export declare const DueDate: import("runtypes").Intersect<[Record<{
     isRecurring: Boolean;
@@ -9,15 +9,47 @@ export declare const DueDate: import("runtypes").Intersect<[Record<{
     timezone: Union<[String, Literal<null>]>;
 }, false>]>;
 
+export type DueDate = Static<typeof DueDate>;
+
+export type ID = string;
+export type ProjectID = string;
+export type SectionID = string;
+export type Priority = number;
+
 export interface TaskProperties {
-    id: number;
-    title: string;
+    id: ID;
+    content: string;
+    priority: Priority;
     description: string;
-    due: DueDate;
-    priority: number;
+    order: number;
+    projectID: ProjectID;
+    sectionID: SectionID;
+    labels: string[];
     completed: boolean;
-    tags: string[];
-    parentId: number;
-    sectionId: number;
+
+    parent?: TaskProperties;
+    children: TaskProperties[];
+
+    due: DueDate;
     filePath: string;
 }
+
+export class ObsidianTask implements TaskProperties {
+    public id: ID;
+    public content: string;
+    public priority: Priority;
+    public description: string;
+    public order: number;
+    public projectID: ProjectID;
+    public sectionID: SectionID;
+    public labels: string[];
+    public completed: boolean;
+
+    public parent?: ObsidianTask;
+    public children: ObsidianTask[];
+
+    public due: DueDate;
+    public filePath: string;
+
+}
+
