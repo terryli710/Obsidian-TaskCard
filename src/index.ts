cbb4de628048660d9ccab7a8123bea064a935528
terryli710 <li.terry710@gmail.com>
2023-07-28 00:37:00 -0700

    ✨ first commit: file structure

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { App, Plugin } from "obsidian";
+import type { TaskCardSettings } from "./settings";
+
+
+export default class TaskCardPlugin extends Plugin {
+    public settings: TaskCardSettings;
+    private taskFormatter: 
+}
---
+++ src/log.ts
@@ -0,0 +1,11 @@
+
+const winston = require('winston');
+
+const logConfiguration = {
+    'transports': [
+        new winston.transports.Console()
+    ]
+};
+
+export const logger = winston.createLogger(logConfiguration);
+
