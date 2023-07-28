d0addfe66b4de54bcf11544371d727d20eee4d9c
terryli710 <li.terry710@gmail.com>
2023-07-28 16:02:04 -0700

    ✨ task card

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { App, Plugin } from "obsidian";
+import { App, Plugin, PluginManifest } from "obsidian";
 import type { TaskCardSettings } from "./settings";
 
 
 export default class TaskCardPlugin extends Plugin {
     public settings: TaskCardSettings;
-    private taskFormatter: 
+    // TODO: more properties
+    constructor(app: App, pluginManifest: PluginManifest) {
+        super(app, pluginManifest);
+    }
+
+    async onload() {
+        
+    }
 }
---
+++ src/ui/TaskCard.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { writable } from 'svelte/store';
+  
+    // replace with actual task
+    let task = writable({
+      content: 'Task content',
+      description: 'Task description',
+      labels: ['label1', 'label2'],
+      completed: false,
+    });
+  
+    function toggleComplete() {
+      task.update(t => {
+        t.completed = !t.completed;
+        return t;
+      });
+    }
+  </script>
+  
+  <div class="card">
+    <div class="task-checkbox" on:click={toggleComplete}>
+      {$task.completed ? '[x]' : '[ ]'}
+    </div>
+    <div class="task-content">
+      <div class="task-title">{$task.content}</div>
+      {#if $task.description}
+        <div class="task-description">{$task.description}</div>
+      {/if}
+      <div class="task-labels">
+        {#each $task.labels as label}
+          <span class="task-label">#{label}</span>
+        {/each}
+      </div>
+    </div>
+  </div>
