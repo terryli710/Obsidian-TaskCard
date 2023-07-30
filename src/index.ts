71f6795880063d97eac00d6ea1e64b85e101d7bd
terryli710 <li.terry710@gmail.com>
2023-07-30 10:39:38 -0700

    ⚡️ set up winston

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import { App, Plugin, PluginManifest } from 'obsidian';
+import { App, Plugin } from 'obsidian';
+import type { PluginManifest } from 'obsidian';
 import type { TaskCardSettings } from './settings';
+import { DefaultSettings } from './settings';
 import { EmojiPostProcessor } from './renderer/injector';
+import { logger } from './log';
 
 export default class TaskCardPlugin extends Plugin {
   public settings: TaskCardSettings;
   // TODO: more properties
   constructor(app: App, pluginManifest: PluginManifest) {
     super(app, pluginManifest);
+    DefaultSettings.subscribe((settings) => {
+      logger.debug('Settings updated', settings);
+      this.settings = settings;
+    })
   }
 
   async onload() {
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,5 @@
-import { Writable, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 
 export interface TaskCardSettings {
   syncSetting: any; // Todoist account info + other possible synced platforms
