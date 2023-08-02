443b284724e95cdd6ef62df7bf7b61ca98ac9513
terryli710 <li.terry710@gmail.com>
2023-08-02 10:14:15 -0700

    🎨 adjust shadow color

--- src/ui/TaskItem.svelte
+++ src/ui/TaskItem.svelte
@@ -1,20 +1,18 @@
 <script>
     import { logger } from "../log";
-    import { darkenHexColor } from "../utils/colorConverter";
     import TaskCard from "./TaskCard.svelte";
   
     export let taskItemEl;
     const taskEl = taskItemEl.querySelector('.obsidian-taskcard');
     logger.debug(`taskEl: ${taskEl.innerHTML}`);
   
-    const style = getComputedStyle(document.body);
-    const color = style.getPropertyValue('--background-secondary').trim();
-    const darkenColor = darkenHexColor(color, 0.2);
+    // const style = getComputedStyle(document.body);
+    // const color = style.getPropertyValue('--background-secondary').trim();
+    // const darkenColor = darkenHexColor(color, 0.2);
 
     taskItemEl.innerHTML = "";
-  </script>
-  
-  <div class="obsidian-taskcard task-list-item" style="box-shadow: 0px 0px 10px {darkenColor};">
-    <TaskCard {taskEl} />
-  </div>
-  
+</script>
+
+<div class="obsidian-taskcard task-list-item">
+<TaskCard {taskEl} />
+</div>
