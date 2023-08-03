9fe25197926a448dadf34a7b86b2c4413979c9c7
terryli710 <li.terry710@gmail.com>
2023-08-02 23:08:00 -0700

    🎨 color naming

--- tests/colorConverter.test.ts
+++ tests/colorConverter.test.ts
@@ -1,59 +1,59 @@
-import { darkenHexColor, darkenRGBColor, hexToRgb, rgbToHex } from "../src/utils/colorConverter";
+import { darkenHEXColor, darkenRGBColor, HEXToRGB, RGBToHEX } from "../src/utils/colorConverter";
 
 describe("Color Conversion Utilities", () => {
-    describe("hexToRgb", () => {
-        it("should correctly convert hex to rgb", () => {
-            const rgb = hexToRgb("#FFFFFF");
-            expect(rgb).toEqual({ r: 255, g: 255, b: 255 });
+    describe("HEXToRGB", () => {
+        it("should correctly convert HEX to RGB", () => {
+            const RGB = HEXToRGB("#FFFFFF");
+            expect(RGB).toEqual({ r: 255, g: 255, b: 255 });
         });
 
-        it("should return null for invalid hex code", () => {
-            const rgb = hexToRgb("#GGGGGG");
-            expect(rgb).toBeNull();
+        it("should return null for invalid HEX code", () => {
+            const RGB = HEXToRGB("#GGGGGG");
+            expect(RGB).toBeNull();
         });
     });
 
-    describe("rgbToHex", () => {
-        it("should correctly convert rgb to hex", () => {
-            const hex = rgbToHex({ r: 255, g: 255, b: 255 });
-            expect(hex).toEqual("#ffffff");
+    describe("RGBToHEX", () => {
+        it("should correctly convert RGB to HEX", () => {
+            const HEX = RGBToHEX({ r: 255, g: 255, b: 255 });
+            expect(HEX).toEqual("#ffffff");
         });
     });
 
     describe("darkenRGBColor", () => {
         it("should darken the color correctly", () => {
-            const rgb = darkenRGBColor({ r: 255, g: 255, b: 255 }, 0.5);
-            expect(rgb).toEqual({ r: 127, g: 127, b: 127 });
+            const RGB = darkenRGBColor({ r: 255, g: 255, b: 255 }, 0.5);
+            expect(RGB).toEqual({ r: 127, g: 127, b: 127 });
         });
 
         it("should not darken beyond 0", () => {
-            const rgb = darkenRGBColor({ r: 255, g: 255, b: 255 }, 1.5);
-            expect(rgb).toEqual({ r: 0, g: 0, b: 0 });
+            const RGB = darkenRGBColor({ r: 255, g: 255, b: 255 }, 1.5);
+            expect(RGB).toEqual({ r: 0, g: 0, b: 0 });
         });
     });
 
-    describe("darkenHexColor", () => {
+    describe("darkenHEXColor", () => {
         it("should darken the color correctly", () => {
-            const hex = darkenHexColor("#FFFFFF", 0.5);
-            expect(hex).toEqual("#7f7f7f");
+            const HEX = darkenHEXColor("#FFFFFF", 0.5);
+            expect(HEX).toEqual("#7f7f7f");
         });
 
         it("should not darken beyond 0", () => {
-            const hex = darkenHexColor("#FFFFFF", 1.5);
-            expect(hex).toEqual("#000000");
+            const HEX = darkenHEXColor("#FFFFFF", 1.5);
+            expect(HEX).toEqual("#000000");
         });
 
-        it("should return original color for invalid hex code", () => {
-            const hex = darkenHexColor("#GGGGGG", 0.5);
-            expect(hex).toEqual("#GGGGGG");
+        it("should return original color for invalid HEX code", () => {
+            const HEX = darkenHEXColor("#GGGGGG", 0.5);
+            expect(HEX).toEqual("#GGGGGG");
         });
 
         it("another test case for darken color", () => {
-            const hex = darkenHexColor("#F6F6F6", 0.2);
-            expect(hex).toEqual("#c4c4c4");
+            const HEX = darkenHEXColor("#F6F6F6", 0.2);
+            expect(HEX).toEqual("#c4c4c4");
         });
     });
 
 
-    
+
 });
