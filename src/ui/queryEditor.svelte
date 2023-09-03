2d537746ff99607da02de2cbf98a6f6d2cecf92b
terryli710 <li.terry710@gmail.com>
2023-09-03 15:08:29 -0700

    🎨 attempts to update editor

--- src/ui/queryEditor.svelte
+++ src/ui/queryEditor.svelte
@@ -6,7 +6,7 @@
     import { logger } from "../utils/log";
 
     export let options: TaskQueryOptions;
-    export let query: MultipleAttributeTaskQuery = {
+    export let intialQuery: MultipleAttributeTaskQuery = {
         priorityQuery: [],
         projectQuery: [],
         labelQuery: [],
@@ -14,32 +14,20 @@
         dueDateTimeQuery: ['', ''],
         filePathQuery: '',
     };
+
+    let query: MultipleAttributeTaskQuery = intialQuery;
+
     export let querySyncManager: QuerySyncManager;
 
-    let selectedCompleted = [];
-    let selectedPriority = [];
+    let numberOfToggles: number = 0;
 
-    function toggleSelected(type, value) {
-        console.log(`toggleSelected: type = ${type}, value = ${value}`);
-        let selectedArray;
-        if (type === 'completed') {
-            selectedArray = selectedCompleted;
-            query.completedQuery = selectedArray;
-        } else if (type === 'priority') {
-            selectedArray = selectedPriority;
-            query.priorityQuery = selectedArray;
-        }
+    let priorityMapping = {
+        1: 'High',
+        2: 'Medium',
+        3: 'Low',
+        4: 'None'
+    };
 
-        const index = selectedArray.indexOf(value);
-        if (index === -1) {
-            selectedArray.push(value);
-        } else {
-            selectedArray.splice(index, 1);
-        }
-    }
-
-
-    logger.debug(`queryEditor.svelte: query = ${JSON.stringify(query)}, options = ${JSON.stringify(options)}`);
     // Function to save the query
     function saveQuery() {
         // Post-processing to convert empty arrays or strings to null
@@ -50,131 +38,166 @@
                 query[key] = null;
             }
         }
-        logger.debug(`queryEditor.svelte: query = ${JSON.stringify(query)}`);
         querySyncManager.updateTaskQueryToFile(query);
     }
+
+    function toggleSelected(type, value, event) {
+        // Check if the event is a "select event"
+        if (!isSelectEvent(event)) { 
+            return; 
+        }
+        numberOfToggles++;
+
+        // Determine which query array to modify
+        let selectedArray;
+        if (type === 'completed') {
+            selectedArray = query.completedQuery;
+        } else if (type === 'priority') {
+            selectedArray = query.priorityQuery;
+        }
+
+        // If the array is undefined, exit the function
+        if (!selectedArray) {
+            console.error("Invalid type provided:", type);
+            return;
+        }
+
+        // Check if the value already exists in the array
+        const index = selectedArray.indexOf(value);
+
+        // Add or remove the value from the array
+        if (index === -1) {
+            selectedArray.push(value);
+        } else {
+            selectedArray.splice(index, 1);
+        }
+
+        // If the array is empty, clear the event target value (optional)
+        if (selectedArray.length === 0) {
+            event.target.value = '';
+        }
+
+        // Log for debugging
+        logger.debug("Updated query:", query);
+    }
+
+    function isSelectedChoice(type, value) {
+        logger.debug(`query.completedQuery.includes(value): ${query.completedQuery.includes(value)}`);
+        if (type === 'completed') {
+            return query.completedQuery.includes(value);
+        } else if (type === 'priority') {
+            return query.priorityQuery.includes(value);
+        }
+    }
+
+    function isSelectEvent(event) {
+        // return true if is mount click or "Enter" key
+        return event.type === 'click' || event.key === 'Enter';
+    }
+
 </script>
 
-<div class="form-container">
+
+<ul class="query-editor">
     <!-- Completed -->
-    <div class="form-group">
-        <label class="title-completed" for="completed">Completed:</label>
-        <div id="completed" class="button-group">
-            <button class="toggle-button {selectedCompleted.includes('Yes') ? 'selected' : ''}" on:click={() => toggleSelected('completed', 'yes')}>Yes</button>
-            <button class="toggle-button {selectedCompleted.includes('No') ? 'selected' : ''}" on:click={() => toggleSelected('completed', 'no')}>No</button>
+    <li class="query-section">
+        <div class="query-section-title">Completed:</div>
+        <div class="fixed-choice-selections">
+            <ul class="fixed-choice-list">
+                <li class="fix-chioce-item">
+                    <button 
+                        class="fix-chioce-item" class:is-selected={isSelectedChoice('completed', true)}
+                        on:click={(evt) => toggleSelected('completed', true, evt)}
+                        on:keydown={(evt) => toggleSelected('completed', true, evt)}
+                    >yes</button>
+                </li>
+                <li class="fix-chioce-item">
+                    <button 
+                        class="fix-chioce-item"
+                        on:click={(evt) => toggleSelected('completed', false, evt)}
+                        on:keydown={(evt) => toggleSelected('completed', false, evt)}
+                    >no</button>
+                </li>
+            </ul>
         </div>
-    </div>
+    </li>
 
     <!-- Priority -->
-    <div class="form-group">
-        <label class="title-priority" for="priority">Priority:</label>
-        <div id="priority" class="button-group">
-            <button class="toggle-button {selectedPriority.includes(1) ? 'selected' : ''}" on:click={() => toggleSelected('priority', 1)}>High</button>
-            <button class="toggle-button {selectedPriority.includes(2) ? 'selected' : ''}" on:click={() => toggleSelected('priority', 2)}>Medium</button>
-            <button class="toggle-button {selectedPriority.includes(3) ? 'selected' : ''}" on:click={() => toggleSelected('priority', 3)}>Low</button>
-            <button class="toggle-button {selectedPriority.includes(4) ? 'selected' : ''}" on:click={() => toggleSelected('priority', 4)}>None</button>
+    <li class="query-section">
+        <div class="query-section-title">Priority:</div>
+        <div class="fixed-choice-selections">
+            <ul class="fixed-choice-list">
+                {#each [1, 2, 3, 4] as priority}
+                    <li class="fix-chioce-item">
+                        <button 
+                            class="{query.priorityQuery.includes(priority) ? 'is-selected' : ''}"
+                            on:click={(evt) => toggleSelected('priority', priority, evt)}
+                            on:keydown={(evt) => toggleSelected('priority', priority, evt)}
+                        >
+                            {priorityMapping[priority]}
+                        </button> 
+                    </li>
+                {/each}
+            </ul>
         </div>
-    </div>
+    </li>
 
     <!-- Project -->
-    <div class="form-group">
-        <label for="project">Project:</label>
-        <select id="project" multiple bind:value={query.projectQuery}>
-            {#if options?.projectOptions && options.projectOptions.length > 0}
-                {#each options.projectOptions as project}
-                <option value={project}>{project.name}</option>
-                {/each}
-            {/if}
-        </select>
-    </div>
+    <li class="query-section">
+        <div class="query-section-title">Project:</div>
+        <div class="project-choice-selections">
+            <ul class="project-choice-list">
+                {#if options?.projectOptions && options.projectOptions.length > 0}
+                    {#each options.projectOptions as project}
+                    <li class="project-chioce-item">{project.name}</li>
+                    {/each}
+                {/if}
+            </ul>
+        </div>
+    </li>
 
     <!-- Label -->
-    <div class="form-group">
-        <label for="label">Label:</label>
-        <select id="label" multiple bind:value={query.labelQuery}>
-            {#if options?.labelOptions && options.labelOptions.length > 0}
-                {#each options.labelOptions as label}
-                <option value={label}>{label}</option>
-                {/each}
-            {/if}
-        </select>
-    </div>
+    <li class="query-section">
+        <div class="query-section-title">Label:</div>
+        <div class="label-choice-selections">
+            <ul class="label-choice-list">
+                {#if options?.labelOptions && options.labelOptions.length > 0}
+                    {#each options.labelOptions as label}
+                    <li class="label-chioce-item">{label}</li>
+                    {/each}
+                {/if}
+            </ul>
+        </div>
+    </li>
 
-    <!-- Due Date Time -->
-    <div class="form-group">
-        <label for="dueDateTime">Due Date Time:</label>
-        <input id="dueDateTime" type="text" placeholder="Time Picker Here" bind:value={query.dueDateTimeQuery} />
-    </div>
+    <li class="query-section">
+        <div class="query-section-title">Due Date Time:</div>
+        <div class="due-datetime-input-area">
+            <div class="query-input-area">
+                <input type="text" placeholder="Start Time" bind:value={query.dueDateTimeQuery[0]} />
+                <input type="text" placeholder="End Time" bind:value={query.dueDateTimeQuery[1]} />
+        </div>
+    </li>
 
-    <!-- File Path -->
-    <div class="form-group">
-        <label for="filePath">File Path:</label>
-        <input id="filePath" type="text" bind:value={query.filePathQuery} />
-    </div>
+    <li class="query-section">
+        <div class="query-section-title">File Path:</div>
+        <div class="query-input-area">
+            <input type="text" placeholder="File Path" bind:value={query.filePathQuery} />
+        </div>
+    </li>
 
-    <!-- Save Button -->
-    <div class="form-group">
+    <class class="button-menu">
         <button on:click={saveQuery}>Save</button>
-    </div>
-</div>
+    </class>
+
+
+</ul>
+
 
 
 <style>
-    /* General Styles */
-    .form-container {
-        width: 100%;
-        max-width: 600px;
-        margin: auto;
-        padding: 20px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    .is-selected {
+        background-color: blue;
     }
-
-    .form-group {
-        margin-bottom: 20px;
-    }
-
-    label {
-        display: inline-block;
-        margin-bottom: 8px;
-        font-weight: bold;
-    }
-
-    /* Button Group Styles */
-    .button-group {
-        display: flex;
-        gap: 10px;
-    }
-
-    .toggle-button {
-        padding: 10px;
-        border: 1px solid #ccc;
-        border-radius: 20px;
-        background: #f1f1f1;
-        cursor: pointer;
-        transition: background 0.3s ease;
-    }
-
-    .toggle-button.selected {
-        background: #007bff;
-        color: white;
-    }
-
-    /* Save Button Styles */
-    .save-button {
-        padding: 10px 20px;
-        background-color: #007bff;
-        color: white;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-
-    .save-button:hover {
-        background-color: #0056b3;
-    }
-
-    .title-completed, .title-priority, .title-project, .title-label, .title-dueDateTime, .title-filePath {
-        font-size: 1.2em;
-        color: #333;
-    }
+    
 </style>
