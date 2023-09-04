bd052f2768bdb4a9a9e43a0192ee8feb296eab50
terryli710 <li.terry710@gmail.com>
2023-09-04 11:08:49 -0700

    ✨ obisidan-calendar attempt

---
+++ src/ui/calendar/Calendar.svelte
@@ -0,0 +1,77 @@
+<svelte:options immutable />
+
+<script lang="ts">
+  import type { Moment } from "moment";
+  import moment from "moment";
+  import {
+    Calendar as CalendarBase,
+    ICalendarSource,
+    IWeekStartOption,
+    configureGlobalMomentLocale,
+  } from "obsidian-calendar-ui";
+  import { createEventDispatcher, onDestroy } from "svelte";
+
+
+  let today: Moment;
+
+  $: today = getToday("system-default", "sunday");
+
+  export let displayedMonth: Moment = today;
+  export let sources: ICalendarSource[] = [];
+  export let onHoverDay: (date: Moment, targetEl: EventTarget) => boolean = () => true;
+  export let onHoverWeek: (date: Moment, targetEl: EventTarget) => boolean = () => true;
+//   export let onClickDay: (date: Moment, isMetaPressed: boolean) => boolean = () => true;
+  export let onClickWeek: (date: Moment, isMetaPressed: boolean) => boolean = () => true;
+  export let onContextMenuDay: (date: Moment, event: MouseEvent) => boolean = () => true;
+  export let onContextMenuWeek: (date: Moment, event: MouseEvent) => boolean = () => true;
+
+  let selectedDate: Moment = displayedMonth.clone();
+
+  export function tick() {
+    today = window.moment();
+  }
+
+
+
+  function getToday(localeOverride: string, weekStart: IWeekStartOption) {
+    configureGlobalMomentLocale(localeOverride, weekStart);
+    return window.moment();
+  }
+
+  // 1 minute heartbeat to keep `today` reflecting the current day
+  let heartbeat = setInterval(() => {
+    tick();
+
+    const isViewingCurrentMonth = displayedMonth.isSame(today, "day");
+    if (isViewingCurrentMonth) {
+      // if it's midnight on the last day of the month, this will
+      // update the display to show the new month.
+      displayedMonth = today;
+    }
+  }, 1000 * 60);
+
+  onDestroy(() => {
+    clearInterval(heartbeat);
+  });
+
+  const selectDateDispatch = createEventDispatcher();
+  $: selectDateDispatch("selected", selectedDate);
+
+  function onClickDay(date: Moment, isMetaPressed: boolean) {
+    selectedDate = date;
+  }
+
+</script>
+
+
+<CalendarBase
+  {today}
+  {onHoverDay}
+  {onHoverWeek}
+  {onContextMenuDay}
+  {onContextMenuWeek}
+  {onClickDay}
+  {onClickWeek}
+  bind:displayedMonth
+  localeData={today.localeData()}
+/>
--- src/ui/queryEditor.svelte
+++ src/ui/queryEditor.svelte
@@ -4,9 +4,11 @@
     import { MultipleAttributeTaskQuery } from "../query/cache";
     import { QuerySyncManager, TaskQueryOptions } from "../query/querySyncManager";
     import { logger } from "../utils/log";
+    import Calendar from './calendar/Calendar.svelte';
     import FixedOptionsSelect from "./selections/FixedOptionsSelect.svelte";
     import ProjectSelection from "./selections/ProjectSelection.svelte";
     import TagSelect from "./selections/TagSelect.svelte";
+    import moment from 'moment';
 
     export let options: TaskQueryOptions;
     export let query: MultipleAttributeTaskQuery = {
@@ -17,9 +19,13 @@
         dueDateTimeQuery: ['', ''],
         filePathQuery: '',
     };
-
-    logger.debug(`query: ${JSON.stringify(query)}`);
     export let querySyncManager: QuerySyncManager;
+
+    let startDate = new Date(query.dueDateTimeQuery[0]);
+    let endDate = new Date(query.dueDateTimeQuery[1]);
+
+    let startMoment = moment(startDate);
+    let endMoment = moment(endDate);
 
     // Function to save the query
     function saveQuery() {
@@ -33,7 +39,6 @@
         }
         querySyncManager.updateTaskQueryToFile(query);
     }
-
         
     // Function to handle selection updates
     function handleSelection(event, queryName) {
@@ -51,6 +56,17 @@
         }
     }
 
+    function handleDateSelection(event, queryName) {
+        const selectedValue = event.detail;
+        logger.debug(`Date selected value: ${selectedValue}`);
+        if (queryName = 'startDate') {
+            query.dueDateTimeQuery[0] = selectedValue;
+        } else if (queryName = 'endDate') {
+            query.dueDateTimeQuery[1] = selectedValue;
+        }
+
+    }
+
     // choices
     const completedChoices = [
         { value: true, displayText: 'Yes' }, 
@@ -63,29 +79,63 @@
         { value: 4, displayText: 'None' }
     ];
 
+
 </script>
 
 
 <ul class="query-editor">
     <!-- Completed -->
-    <FixedOptionsSelect title="Completed" description="To include completed or uncompleted tasks" choices={completedChoices} initialChoices={query.completedQuery} on:selected={(evt) => handleSelection(evt, 'completed')} />
+    <FixedOptionsSelect 
+        title="Completed" 
+        description="To include completed or uncompleted tasks" 
+        choices={completedChoices} 
+        initialChoices={query.completedQuery} 
+        on:selected={(evt) => handleSelection(evt, 'completed')} 
+    />
 
     <!-- Priority -->
-    <FixedOptionsSelect title="Priority" description="To filter teh priority of the task" choices={priorityChoices} initialChoices={query.priorityQuery} on:selected={(evt) => handleSelection(evt, 'priority')} />
+    <FixedOptionsSelect 
+        title="Priority" 
+        description="To filter teh priority of the task" 
+        choices={priorityChoices} 
+        initialChoices={query.priorityQuery} 
+        on:selected={(evt) => handleSelection(evt, 'priority')} 
+    />
 
     <!-- Project -->
-    <ProjectSelection title="Project" description="To filter by project" choices={options?.projectOptions} initialChoices={query.projectQuery} on:selected={(evt) => handleSelection(evt, 'project')} />
+    <ProjectSelection 
+        title="Project" 
+        description="To filter by project" 
+        choices={options?.projectOptions} 
+        initialChoices={query.projectQuery} 
+        on:selected={(evt) => handleSelection(evt, 'project')} 
+    />
 
     <!-- Label -->
-    <TagSelect title="Label" description="To filter by label" choices={options?.labelOptions} initialChoices={query.labelQuery} on:selected={(evt) => handleSelection(evt, 'label')} />
+    <TagSelect 
+        title="Label" 
+        description="To filter by label" 
+        choices={options?.labelOptions} 
+        initialChoices={query.labelQuery} 
+        on:selected={(evt) => handleSelection(evt, 'label')} 
+    />
 
-
+    <!-- Due DateTime -->
     <li class="query-section">
-        <div class="query-section-title">Due Date Time:</div>
-        <div class="due-datetime-input-area">
-            <div class="query-input-area">
-                <input type="text" placeholder="Start Time" bind:value={query.dueDateTimeQuery[0]} />
-                <input type="text" placeholder="End Time" bind:value={query.dueDateTimeQuery[1]} />
+        <div class="header">
+            <div class="inline-title-wrapper">
+                <div class="inline-title">Due Date</div>
+                <div class="inline-description">To filter by due date</div>
+            </div>
+            <div class="separator"></div>
+            <Calendar 
+                displayedMonth={startMoment || undefined}
+                on:selected={(evt) => handleDateSelection(evt, 'startDate')}
+            />
+            <Calendar 
+                displayedMonth={endMoment || undefined}
+                on:selected={(evt) => handleDateSelection(evt, 'endDate')}
+            />
         </div>
     </li>
 
@@ -106,5 +156,6 @@
 
 
 <style>
-    
+
+
 </style>
